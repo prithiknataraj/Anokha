74174982744feeb2f9edae9eea35fea94db97117
Prith <93419588+prithiknataraj@users.noreply.github.com>
2024-07-26 11:03:15 +0530

    updation

--- app.py
+++ app.py
@@ -1,60 +1,67 @@
-from flask import Flask, jsonify, request, render_template
-from pymongo  import MongoClient
+from flask import Flask, jsonify, request, render_template, redirect, url_for
+from pymongo import MongoClient
 
-app= Flask(__name__)
+app = Flask(__name__)
 
-# _________________________Connecting MongoDB_________________________
-client= MongoClient("mongodb+srv://prithik:Indian@cluster0.gyp783r.mongodb.net/?retryWrites=true&w=majority&appName=cluster0")
-db= client["anokha"]
-users= db["users"]
+# Connecting to MongoDB
+client = MongoClient("mongodb+srv://prithik:Indian@cluster0.gyp783r.mongodb.net/?retryWrites=true&w=majority&appName=cluster0")
+db = client["anokha"]
+users = db["users"]
 
-# _________________________Connecting REACT_________________________
-# Login Page
-
+# Index Page
 @app.route("/")
 def index():
     return render_template("index.html")
-    
 
-@app.route("/login", methods=["POST"])
+# Login Page
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    data = request.json
+    if request.method == "POST":
+        data = request.json
+        username = data.get('username')
+        password = data.get('password')
+        
+        user = users.find_one({"email": username, "password": password})
+        
+        if user:
+            users.update_one({"admin_email": "yukthi@gmail.com"}, {"$set": {"current_user": username}}, upsert=True)
+            return jsonify({"response": "success"}), 200  # Respond with success
+        return jsonify({"response": "nologin"}), 200
+    
+    return render_template("login.html")
+
+# SignUp Page
+@app.route("/signup", methods=["GET", "POST"])
+def signup_page():
+    return render_template("signup.html")
+
+@app.route("/api/signup", methods=["POST"])
+def signup():
+    data = request.json    
     username = data.get('username')
     password = data.get('password')
+    phone_number = data.get('phoneNumber')
+    email = data.get('email')
     
-    db.users.delete_one({"admin_email":"yukthi@gmail.com"})
-    db.users.insert_one({"admin_email":"yukthi@gmail.com", "current_user": username})
-    
-    user= db.users.find_one({"email": username, "password": password})
-    
-    if user:
-        return jsonify({"response": "login"}), 200
-
-    return jsonify({"response": "nologin"}), 200
-
-#SignUp Page
-@app.route("/api/signup", methods=["POST"])
-def signup():    
-    data= request.json    
-    username= data.get('username')
-    password= data.get('password')
-    preferred_language= data.get('preferredLanguage')
-    age= data.get('age')
-    gender= data.get('gender')
-    phone_number= data.get('phoneNumber')
-    email= data.get('email')
-    disability= data.get('disabilities')
-    
-    db.users.delete_one({"admin_email":"yukthi@gmail.com"})
-    db.users.insert_one({"admin_email":"yukthi@gmail.com", "current_user": username})
-    
-    if db.users.find_one({'username': username}):
+    if users.find_one({'email': email}):
         return jsonify({"response": "Mail ID already registered"})
-
-    user_data = {'Name': username, 'password': password, 'age':age,'preferred_language':preferred_language, 'gender':gender, "phone_number": phone_number, "email": email, "disability": disability}
-    db.users.insert_one(user_data)
+    
+    user_data = {
+        'Name': username, 
+        'password': password,  
+        "phone_number": phone_number, 
+        "email": email
+    }
+    users.insert_one(user_data)
+    
+    users.update_one({"admin_email": "yukthi@gmail.com"}, {"$set": {"current_user": username}}, upsert=True)
     
     return jsonify({"response": "signup"})
 
-if __name__== "__main__":
-    app.run(debug= True)
+# Home Page
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
